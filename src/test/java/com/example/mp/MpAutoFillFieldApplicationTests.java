d99d61918cb96d64dabedf8aefe7794b9c796f11
XunLiang <windyhill1999@qq.com>
2024-10-23 01:19:08 +0800

    .

--- src/test/java/com/example/mp/MpAutoFillFieldApplicationTests.java
+++ src/test/java/com/example/mp/MpAutoFillFieldApplicationTests.java
@@ -14,60 +14,55 @@ import java.util.concurrent.locks.Lock;
 @SpringBootTest
 class MpAutoFillFieldApplicationTests {
 
-	@Autowired
-	private StudentMapper studentMapper;
+    @Autowired
+    private StudentMapper studentMapper;
 
-	@Test
-	synchronized void contextLoads() {
+    @Test
+    synchronized void contextLoads() {
+        try {
+            Student student = new Student();
+            student.setName("yahahah");
 
-		 {
-			try {
-				Student student = new Student();
-				student.setName("yahahah");
+            insert(student);
 
-				insert(student);
+            student = getById(student.getId());
+            log.info(student.toString());
 
-				student = getById(student.getId());
-				log.info(student.toString());
+            this.wait(3000);
 
-				this.wait(3000);
+            student.setUpdateTime(null);
+            delete(student);
 
-				student.setUpdateTime(null);
-				delete(student);
+            student = getById(student.getId());
+            log.info(String.valueOf(student));
 
-				student = getById(student.getId());
-				log.info(String.valueOf(student));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-		}
+    void insert(Student student) {
+        studentMapper.insert(student);
+    }
 
+    Student getById(Long id) {
+        return studentMapper.selectById(id);
+    }
 
-	}
+    void delete(Student student) {
 
-	void insert(Student student) {
-		studentMapper.insert(student);
-	}
+        studentMapper.deleteById(student);
 
-	Student getById(Long id) {
-		return studentMapper.selectById(id);
-	}
+        // studentMapper.deleteByIdWithFill(student);
 
-	void delete(Student student) {
+        UpdateWrapper<Student> wrapper = new UpdateWrapper<>();
+        wrapper.eq("id", student.getId());
+        wrapper.set("deleted", 1);
+        // studentMapper.update(student, wrapper);
+    }
 
-		studentMapper.deleteById(student);
-
-		// studentMapper.deleteByIdWithFill(student);
-
-		UpdateWrapper<Student> wrapper = new UpdateWrapper<>();
-		wrapper.eq("id", student.getId());
-		wrapper.set("deleted", 1);
-		// studentMapper.update(student, wrapper);
-	}
-
-	void delete(Long id) {
-		studentMapper.deleteById(id);
-	}
+    void delete(Long id) {
+        studentMapper.deleteById(id);
+    }
 
 }
